cee80bf81a4375c4d0ae9518aeb01c70fef94043
Mihai Dogaru <dogarumihai2537@gmail.com>
2025-09-22 22:40:24 +0300

    Add JS skel

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
 console.log("Hello world!");
+
+const cards = document.querySelectorAll(".card");
+
+const tabs = document.querySelectorAll(".btn--tab");
+const classSelected = "selected";
+
+let selectedTab = tabs[0].textContent;
+initTabs();
+
+function initTabs() {
+  tabs[0].classList.add(classSelected);
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", handleTabClick);
+  });
+}
+
+function handleTabClick(event) {
+  removeSelectionFromAllTabs();
+
+  event.target.classList.add(classSelected);
+  selectedTab = event.target.textContent;
+
+  updateData(selectedTab);
+}
+
+function removeSelectionFromAllTabs() {
+  tabs.forEach((tab) => {
+    tab.classList.remove(classSelected);
+  });
+}
+
+function updateData(selectedTabName) {
+  console.log(`Updating data: ${selectedTabName}`);
+  console.log(cards);
+
+  // use this to populate cards data
+  // call it from click listener and once the fetch completes
+}
+
+fetch("data.json")
+  .then((response) => {
+    if (!response.ok) return console.log("Oops! Something went wrong!");
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
